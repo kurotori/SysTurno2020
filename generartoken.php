cf18aceea27bf14e179066bc041fc25c28c6e1cd
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-02-19 09:49:46 -0300

    Update generartoken.php

--- generartoken.php
+++ generartoken.php
@@ -3,8 +3,21 @@
 $usuario = "fulano";
 $chequeo = true;
 $tiempoUnix = time();
+$jsonDatos = new stdClass;
 
-echo "$usuario - $chequeo - $tiempoUnix";
+//echo "$usuario - $chequeo - $tiempoUnix <br/>";
+
+$dato_string = "$usuario.$chequeo.$tiempoUnix";
+
+$token = password_hash($dato_string, PASSWORD_BCRYPT);
+
+$jsonDatos->chequeo = $chequeo;
+$jsonDatos->token = $token;
+
+$objJSON = json_encode($jsonDatos);
+
+echo $objJSON;
+
 
 
 ?>
