a286dac8a45d02a5dd3121c5f50b15d227514d8d
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-03-29 00:32:17 -0300

    Create formulario_logout.php

---
+++ webservice/formulario_logout.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta http-equiv='Content-type' content='text/html;charset=UTF-8'>
+    </head>
+    <body>
+        <form id="previo" action="logout/" method="post">
+            
+            <label for="usuario_ci">Usuario</label><br /> 
+            <input id="usuario_ci" name="usuario_ci" type="text" /> <br />
+            
+            <input type="submit" value="Enviar">
+        </form>
+        
+        <script>
+            
+        </script>
+        
+    </body>
+</html>
